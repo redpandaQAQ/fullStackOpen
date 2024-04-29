0cea1927d8174b017eaf9b330ccb7989d8d709b1
Yu <114864805+redpandaQAQ@users.noreply.github.com>
2024-04-29 02:32:59 +0000

    anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState([0,0,0,0,0,0,0,0])
+  let most = points.indexOf(Math.max(...points));
+
+  function getRandomInt(max) {
+    let rng = Math.floor(Math.random() * max)
+    return rng
+  }
+  function vote(n) {
+    let newPoints = [...points]
+    newPoints[n] = newPoints[n] + 1
+    return () => setPoints(newPoints)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br/>
+      has {points[selected]} votes
+      <br/>
+      <button onClick={vote(selected)}>
+        vote
+      </button>
+      <button onClick={()=>setSelected(getRandomInt(7))}>
+        next anecdote
+      </button>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[most]}
+      <br/>
+      has {points[most]} votes
+    </div>
+  )
+}
+
+export default App
